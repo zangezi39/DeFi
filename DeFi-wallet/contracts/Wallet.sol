49cd43d2ac70a0ab2db3f0db7112db0752d92e33
Savely Rosenaur <71658837+zangezi39@users.noreply.github.com>
2022-12-02 20:38:56 +0100

    Add files via upload

---
+++ DeFi-wallet/contracts/Wallet.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.7.3;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import './Compound.sol';
+
+contract Wallet is Compound {
+  address public admin;
+
+
+  constructor(
+    address _comptroller,
+    address _cEthAddress
+  ) Compound(_comptroller, _cEthAddress) {
+    admin = msg.sender;
+  }
+
+
+  function deposit(
+    address cTokenAddress,
+    uint underlyingAmount
+  ) external {
+    address underlyingAddress = getUnderlyingAddress(cTokenAddress);
+    IERC20(underlyingAddress).transferFrom(msg.sender, address(this), underlyingAmount);
+    supply(cTokenAddress, underlyingAmount);
+  }
+
+
+  function withdraw(
+    address cTokenAddress,
+    uint underlyingAmount,
+    address recipient
+  ) onlyAdmin() external {
+    require(
+      getUnderlyingBalance(cTokenAddress) >= underlyingAmount,
+      'insufficient balance'
+    );
+    claimComp();
+    redeem(cTokenAddress, underlyingAmount);
+
+    address underlyingAddress = getUnderlyingAddress(cTokenAddress);
+    IERC20(underlyingAddress).transfer(recipient, underlyingAmount);
+
+    address compAddress = getCompAddress();
+    IERC20 compToken = IERC20(compAddress);
+    uint compAmount = compToken.balanceOf(address(this));
+    compToken.transfer(recipient, compAmount);
+  }
+
+
+  function withdrawEth(
+    uint underlyingAmount,
+    address payable recipient
+  ) onlyAdmin() external {
+    require(
+      getUnderlyingEthBalance() >= underlyingAmount,
+      'insufficient balance'
+    );
+    claimComp();
+    redeemEth(underlyingAmount);
+
+    recipient.transfer(underlyingAmount);
+
+    address compAddress = getCompAddress();
+    IERC20 compToken = IERC20(compAddress);
+    uint compAmount = compToken.balanceOf(address(this));
+    compToken.transfer(recipient, compAmount);
+  }
+
+  receive() external payable {
+    supplyEth(msg.value);
+  }
+
+
+  modifier onlyAdmin() {
+    require(msg.sender == admin, 'Admin only');
+    _;
+  }
+
+}
